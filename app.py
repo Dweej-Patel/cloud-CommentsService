e14c9b02b8dad6847140e16b512765bcbb097cf5
Dweej Patel <dweejpatel97@gmail.com>
2020-10-12 20:10:35 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,66 @@
+import json
+
+# Setup and use the simple, common Python logging framework. Send log messages to the console.
+# The application should get the log level out of the context. We will change later.
+#
+
+import os
+import sys
+import platform
+import socket
+
+from flask import Flask, Response
+from flask import request
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+print("*** PYHTHONPATH = " + str(sys.path) + "***")
+
+import logging
+from datetime import datetime
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+# Create the application server main class instance and call it 'application'
+# Specific the path that identifies the static content and where it is.
+app = Flask(__name__, static_url_path='/static', static_folder='./static')
+
+
+@app.route('/')
+def hello_world():
+    rsp = app.send_static_file('index.html')
+    return rsp
+
+
+# This function performs a basic health check. We will flesh this out.
+@app.route("/api/health", methods=["GET"])
+def health_check():
+
+    pf = platform.system()
+
+    rsp_data = { "status": "healthy", "time": str(datetime.now()),
+                 "platform": pf,
+                 "release": platform.release()
+                 }
+
+    if pf == "Darwin":
+        rsp_data["note"]= "For some reason, macOS is called 'Darwin'"
+
+
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+
+    rsp_data["hostname"] = hostname
+    rsp_data["IPAddr"] = IPAddr
+
+    rsp_str = json.dumps(rsp_data)
+    rsp = Response(rsp_str, status=200, content_type="application/json")
+    return rsp
+
+
+if __name__ == '__main__':
+    app.run()
